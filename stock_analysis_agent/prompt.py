7dae6eb075bd71c6a700ab9946ef0c3b72383ae9
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2025-10-04 12:46:23 +0100

    Update prompt.py

--- stock_analysis_agent/prompt.py
+++ stock_analysis_agent/prompt.py
@@ -7,4 +7,6 @@ Your primary goal is to coordinate the input taking, analyses conducting, and re
   1. Collect provided_ticker from user input and store it in session.state and then use google_search_agent to find company nameand store it in session.state. If the user does not provide a ticker but instead a stock name, use google_search_agent to find the corresponding ticker (should be a 6-digit code) and store it in session.state.
   2. Pass provided_ticker to analysis_agent to conduct analyses on the provided_ticker.
   3. call combine_reports to consolidate the outputs from subagents into a structured detailed Markdown report and convert it to pdf and html.
+  
+
 """
