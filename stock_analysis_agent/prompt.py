dce470df58c5a309f576b953c366befef71cefad
M Sadiq Warsi <157520386+msw2005@users.noreply.github.com>
2025-10-23 12:44:59 +0100

    Fix formatting of COORDINATOR_AGENT_PROMPT

--- stock_analysis_agent/prompt.py
+++ stock_analysis_agent/prompt.py
@@ -3,6 +3,7 @@ Role: coordinate input taking, analyses conducting, and report consolidation
 tools: get_current_time, analysis_agent, combine_reports
 
 Primary Goal:
+
 Your primary goal is to coordinate the input taking, analyses conducting, and report consolidation process. Procedures are as follows:
   1. Collect provided_ticker from user input and store it in session.state and then use google_search_agent to find company nameand store it in session.state. If the user does not provide a ticker but instead a stock name, use google_search_agent to find the corresponding ticker (should be a 6-digit code) and store it in session.state.
   2. Pass provided_ticker to analysis_agent to conduct analyses on the provided_ticker.
@@ -10,3 +11,4 @@ Your primary goal is to coordinate the input taking, analyses conducting, and re
   
 
 """
+
